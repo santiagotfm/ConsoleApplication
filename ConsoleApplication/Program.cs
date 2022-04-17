ba146e7643177b05444b78473b17dcb530b4fe5b
santiagotfm <santiagotfm28@gmail.com>
2022-04-16 21:02:59 -0500

    week1

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -10,6 +10,34 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
+
+            string name, location;
+            Console.WriteLine("Enter Name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter Location: ");
+            location = Console.ReadLine();
+            Console.WriteLine($"My name is {name}, " +
+                $"I am from {location}");
+            DateTime today = DateTime.Today;
+            Console.WriteLine("Today is " + today.ToString("d"));
+            DateTime christmas = new DateTime(2022, 12, 25);
+            TimeSpan daysUntilChristmas = christmas.Subtract(today);
+            Console.WriteLine($"There are {daysUntilChristmas.Days} " +
+                $"days left until christmas.");
+
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height);
+            Console.WriteLine("The length of the wood is " +
+            woodLength + " feet");
+            Console.WriteLine("The area of the glass is " +
+            glassArea + " square metres");
+            Console.ReadKey();
         }
     }
 }
